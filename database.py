2c72e56e01e119a285d2a6568fccaa26dbe4560e
clemensjosh03 <clemensjosh03@gmail.com>
2026-04-21 13:34:49 +0200

    database.py

--- database.py
+++ database.py
@@ -1,9 +1,10 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
 
-DATABASE_URL = "sqlite:///./schedules.db"
+DATABASE_URL = "postgresql://neondb_owner:npg_iPSoI3xkbG4t@ep-icy-firefly-amm0pki1-pooler.c-5.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require"
 
-engine = create_engine(DATABASE_URL, connect_args={"check_same_thread": False})
+engine = create_engine(DATABASE_URL)
 
 SessionLocal = sessionmaker(bind=engine)
+
 Base = declarative_base()
