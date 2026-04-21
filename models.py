44f1c33437a49ecef61f83c9fe8ba4e8fd59752e
clemensjosh03 <clemensjosh03@gmail.com>
2026-04-21 14:55:57 +0200

    models.py

--- models.py
+++ models.py
@@ -6,13 +6,8 @@ class User(Base):
     __tablename__ = "users"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True)
+    email = Column(String, unique=True, index=True)
     password = Column(String)
-    reminder_days = Column(Integer, default=3)
-    email_frequency = Column(String, default="daily")
-    alert_days = Column(Integer, default=3)
-    notification_interval = Column(Integer, default=24)  # hours
-    last_sent = Column(DateTime, default=None)  # NEW
 
 
 class Task(Base):
